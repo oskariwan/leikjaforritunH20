abf88664f4021084008ffa3da001646ead8e5b4b
oskar iwan <oskariwan40@gmail.com>
2020-09-29 23:00:41 +0000

    update

---
+++ verkefni1/verkefni1/Assets/Code/PlayerColision.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class PlayerColision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisioninfo){
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
